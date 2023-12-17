89f2892007d7a2a5dc9bbb03272bfd4b4fd5523a
bruno-pequeno <bru.pequeno@gmail.com>
2023-12-17 03:00:17 -0300

    feat: Init form component

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -4,6 +4,7 @@ import { Observable, catchError, of } from 'rxjs';
 import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
 import { Course } from '../model/course';
 import { CoursesService } from './../services/courses.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-courses',
@@ -14,7 +15,7 @@ export class CoursesComponent implements OnInit{
   courses$: Observable<Course[]>;
   displayedColumns = ['name', 'category', 'actions'];
 
-  constructor(private coursesService: CoursesService, public dialog: MatDialog) {
+  constructor(private coursesService: CoursesService, public dialog: MatDialog, private router: Router, private route: ActivatedRoute) {
     this.courses$ = this.coursesService.findAll()
     .pipe(
       catchError(error => {
@@ -32,5 +33,9 @@ export class CoursesComponent implements OnInit{
 
   ngOnInit(): void {
 
+  }
+
+  onAdd(){
+    this.router.navigate(['new'], {relativeTo: this.route})
   }
 }
